ca48efa2df42e5302af1e178290559a64f95c39f
faith2221 <edithbanda014@gmail.com>
2024-05-14 09:28:36 -0700

    Log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+script that reads stdin line by line and computes metrics
+"""
+import re
+
+
+def extract_input(input_line):
+    """
+    Extracts sections of a line of an HTTP request log.
+    """
+    fp = (
+        r'\s*(?P<ip>/S+)\s*',
+        r'\s*\[(?P<date>\d+\-\d+\-\d+:\d+:\d+\.\d+)\]',
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r'\s*(?P<status_code>/S+)',
+        r'\s*(?P<file_size>\d+)'
+    )
+    info = {
+        'status_code': 0,
+        'file_size': 0,
+    }
+    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
+    resp_match = re.fullmatch(log_fmt, input_line)
+    if resp_match is not None:
+        status_code = resp_match.group('status_code')
+        file_size = int(resp_match.group('file_size'))
+        info['status_code'] = status_code
+        info['file_size'] = file_size
+    return info
+
+
+def print_statistics(total_file_size, status_code_stats):
+    """
+    Prints the accumulated statistics of the HTTP request log.
+    """
+    print('File size: {:d}'.format(total_file_size), flush=True)
+    for status_code in sorted(status_codes_stats.keys()):
+        num = status_codes_stats.get(status_code, 0)
+        if num > 0:
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
+
+
+def update_metrics(line, total_file_size, status_code_stats):
+    """
+    Updates the metrics from a given HTTP request log.
+    """
+    line_info = extract_input(line)
+    status_code = line_info.get('status_code', '0')
+    if status_code in status_code_stats.keys():
+        status_codes_stats[status_code] += 1
+    return total_file_size + line_info['file_size']
