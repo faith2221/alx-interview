3bb899d5df346784f2bb0c9b9ea9f6875da6011b
faith2221 <edithbanda014@gmail.com>
2024-05-15 07:39:53 -0700

    Corrections

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -50,3 +50,36 @@ def update_metrics(line, total_file_size, status_code_stats):
     if status_code in status_code_stats.keys():
         status_codes_stats[status_code] += 1
     return total_file_size + line_info['file_size']
+
+
+def run():
+    """
+    Starts the log parser.
+    """
+    line_num = 0
+    total_file_size = 0
+    status_code_stats = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
+    try:
+        while True:
+            line = input()
+            total_file_size = update_metrics(
+                line,
+                total_file_size,
+                status_code_stats,
+            )
+            line_num += 1
+            if line_num % 10 == 0:
+                print_statistics(total_file_size, status_code_stats)
+
+
+if __name__ =='__main__':
+    run()
